2fe5392bdd42ce27cd9c27e711bc955b619ad3dc
Ahmad Shafiq <mahmadshafiq786@gmail.com>
2020-02-20 13:39:23 -0800

    Switched to PG

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,42 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213041715) do
+ActiveRecord::Schema.define(version: 2016_12_13_041715) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "user_providers", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.string   "provider",   null: false
-    t.string   "uid",        null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                                       null: false
-    t.string   "crypted_password"
-    t.string   "salt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "activation_state"
-    t.string   "activation_code"
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "activation_state"
+    t.string "activation_code"
     t.datetime "activation_code_expires_at"
-    t.string   "remember_me_token"
+    t.string "remember_me_token"
     t.datetime "remember_me_token_expires_at"
-    t.string   "reset_password_token"
+    t.string "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
     t.datetime "last_login_at"
     t.datetime "last_logout_at"
     t.datetime "last_activity_at"
-    t.integer  "failed_logins_count",             default: 0
+    t.integer "failed_logins_count", default: 0
     t.datetime "lock_expires_at"
-    t.string   "type"
-    t.string   "last_login_from_ip_address"
+    t.string "type"
+    t.string "last_login_from_ip_address"
+    t.index ["activation_code"], name: "index_users_on_activation_code"
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at"
+    t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
   end
-
-  add_index "users", ["activation_code"], name: "index_users_on_activation_code"
-  add_index "users", ["email"], name: "index_users_on_email"
-  add_index "users", ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at"
-  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token"
 
 end
